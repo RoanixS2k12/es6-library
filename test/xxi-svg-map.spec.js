5c5b4ad0a1cd93ccf090ecbf46b45556673ef833
Joani COUMA <joani@21communication.com>
2016-03-21 17:37:28 +0100

    chore(testing): Adding code coverage

--- test/xxi-svg-map.spec.js
+++ test/xxi-svg-map.spec.js
@@ -1,33 +1,52 @@
 import chai from 'chai';
-import SVGMap from '../lib/xxi-svg-map.js';
+import SVGMap from '../src';
 
 chai.expect();
 
 const expect = chai.expect;
 
-var lib;
+var map;
 
 describe('Given an instance of my library', function () {
   before(function () {
-    lib = new SVGMap('', {
+    map = new SVGMap('', {
             width : 100,
             height: 200
           });
   });
 
+  describe('when I create a new object', function() {
+    it('should be a new object', () => {
+      var newMap = new SVGMap('', {
+                      width : 100,
+                      height: 200
+                    });
+      expect(map).to.not.be.null;
+    });
+  });
+
   describe('when I need the name', function () {
     it('should return the name', () => {
-      expect(lib.name).to.be.equal('SVGMap');
+      expect(map.name).to.be.equal('SVGMap');
+    });
+  });
+
+  describe('when I need to modify the name', function() {
+    it('should modify the name', () => {
+
+      map.name = 'NEWMap';
+      expect(map.name).to.be.equal('NEWMap');
+
     });
   });
 
   describe('when I need to know what it looks like', function () {
     it('should return its width', function() {
-      expect(lib.width).to.be.equal(100);
+      expect(map.width).to.be.equal(100);
     });
 
     it('should return its height', function() {
-      expect(lib.height).to.be.equal(200);
+      expect(map.height).to.be.equal(200);
     });
   });
 
