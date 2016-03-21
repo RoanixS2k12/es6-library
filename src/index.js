4daaf346761cc872c6ab821f4a087314f7810389
Joani COUMA <joani@21communication.com>
2016-03-21 16:43:08 +0100

    feat(svgMap): Adding the getters width and height to SVGMap
    We introduced a way to get the width and height of the map outside the scope of the module by adding
    to specific getter to the class SVGMap
    
    closes #2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 
 export default class SVGMap {
 
-  constructor() {
-    this._name = 'SVGMap'
+  constructor(selector, { width, height }) {
+    this._name = 'SVGMap';
+    this._width = width;
+    this._height = height;
   }
 
-  get name() {
-    return this._name;
-  }
+  get name() { return this._name; }
+  get width() { return this._width; }
+  get height() { return this._height; }
 
   set name(value) {
     this._name = value;
--- test/xxi-svg-map.spec.js
+++ test/xxi-svg-map.spec.js
@@ -9,11 +9,26 @@ var lib;
 
 describe('Given an instance of my library', function () {
   before(function () {
-    lib = new SVGMap();
+    lib = new SVGMap('', {
+            width : 100,
+            height: 200
+          });
   });
+
   describe('when I need the name', function () {
     it('should return the name', () => {
       expect(lib.name).to.be.equal('SVGMap');
     });
   });
+
+  describe('when I need to know what it looks like', function () {
+    it('should return its width', function() {
+      expect(lib.width).to.be.equal(100);
+    });
+
+    it('should return its height', function() {
+      expect(lib.height).to.be.equal(200);
+    });
+  });
+
 });
