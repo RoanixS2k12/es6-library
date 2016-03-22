b38c7feb0460acd343f5f72edb7e4548b0ed98bc
Joani COUMA <joani@21communication.com>
2016-03-22 16:00:14 +0100

    feat(SVGMap): Load a file and create the dom svg element from it

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import d3 from 'd3';
 require('d3-geo-projection')(d3);
-// import topojson from 'topojson';
+import topojson from 'topojson';
+
+function setupMap(object, width, height) {
+
+  return new Promise(function (resolve, reject) {
+
+    var svg = object
+                .append('svg')
+                .attr('width', width)
+                .attr('height', height);
+
+    resolve(svg);
+
+  });
+
+}
 
 export default class SVGMap {
 
-  constructor(selector, {
+  constructor(object, {
         file,
         width,
         height,
@@ -12,11 +27,15 @@ export default class SVGMap {
         center = [70, -35],
         ...options}) {
 
-    if (selector === '') {
+    if (object === undefined || object === '') {
       throw new Error('There has to be a selector');
     }
 
-    this._selector = selector;
+    if (file === undefined || file === '') {
+      throw new Error('You did not specified a data file');
+    }
+
+    this._object = object;
     this._file = file;
     this._width = width;
     this._height = height;
@@ -27,13 +46,43 @@ export default class SVGMap {
                             .scale(scale);
 
     this._path = d3.geo.path().projection(this.projection);
-
+    this.createSVG = this.createSVG.bind(this);
   }
 
-  get name() { return this._name; }
+  init() {
+    return setupMap(this.object, this.width, this.height)
+      .then((svg) => {
+        this.svg = svg;
+        return svg;
+      });
+  }
+
+  fetchData() {
+    return new Promise((resolve, reject) => {
+      resolve(this.file);
+    });
+  }
+
+  createSVG(world) {
+    var g = this.svg.append('g');
+
+    return new Promise((resolve, reject) => {
+      // resolve(world);
+      g.selectAll('path')
+          .data(topojson.feature(world, world.objects.countries).features)
+          .enter()
+          .append('path')
+          .attr('class', function (d) { return `country country-${d.id % 2}`; })
+          .attr('d', this.path);
+
+      resolve({ map: this, svg: this.svg});
+    });
+  }
+
   get width() { return this._width; }
   get height() { return this._height; }
-  get selector() { return this._selector; }
+  get object() { return this._object; }
   get projection() { return this._projection; }
+  get file() { return this._file; }
 
 }
--- test/xxi-svg-map.spec.js
+++ test/xxi-svg-map.spec.js
@@ -1,7 +1,19 @@
 import chai from 'chai';
+import d3 from 'd3';
+import jsdom from 'jsdom';
 import SVGMap from '../src';
+import sinon from 'sinon';
+
+var xhr = sinon.useFakeXMLHttpRequest();
+var api = require('../data/world-110m.json');
+// var api = '/hello';
 
 chai.expect();
+
+global.document = jsdom.jsdom('<!doctype html><html><body><div id="selector"></div></body></html>');
+global.window = document.parentWindow;
+
+global.XMLHttpRequest = xhr;
 
 const expect = chai.expect;
 
@@ -10,17 +22,37 @@ describe('Given an instance of an SVGMAp', function () {
   var map;
 
   before(function () {
-    map = new SVGMap('#selector', {
+
+    var div = document.querySelector('#selector');
+    var d3elem = d3.select(div);
+
+    map = new SVGMap(d3elem, {
       width: 100,
-      height: 200
+      height: 200,
+      file: api
     });
+
+  });
+
+  describe('browser setup', function () {
+    it('should have a document', () => {
+      expect(document).to.not.be.null;
+    });
+
+    it('should have a div#selector', () => {
+      var query = document.querySelector('div');
+
+      expect(query).to.not.be.null;
+    });
+
   });
 
   describe('when I create a new object', function () {
     it('should be a new object', () => {
       var newMap = new SVGMap('#selector', {
         width: 100,
-        height: 200
+        height: 200,
+        file: api
       });
 
       expect(newMap).to.not.be.null;
@@ -28,12 +60,22 @@ describe('Given an instance of an SVGMAp', function () {
 
     it('should throw an error if there is no selector', function () {
       var fn = function () {
-        let newMap = new SVGMap('', { width: 100, height: 200});
-
-        console.log(newMap);
+        map = new SVGMap('', { width: 100, height: 200});
       };
 
       expect(fn).to.throw(Error);
+    });
+
+    it('should throw if no associated file is specified', function () {
+      var fn = function () {
+        map = new SVGMap('#selector', {
+          width: 100,
+          height: 200
+        });
+      };
+
+      expect(fn).to.throw(Error);
+
     });
 
   });
@@ -48,12 +90,67 @@ describe('Given an instance of an SVGMAp', function () {
       expect(map.height).to.be.equal(200);
     });
 
-    it('should return its selector', function () {
-      expect(map.selector).to.be.equal('#selector');
+    it('should return its object', function () {
+      expect(map.object).to.not.be.null;
     });
 
     it('should return its projection', function () {
       expect(map.projection).to.not.be.null;
+    });
+
+  });
+
+  describe('when I initialize it', function () {
+
+    it('should return a promise if no callback are given', function () {
+      var type = map.init();
+
+      expect(type).to.be.a('promise');
+
+      type.then((world) => {
+        expect(world).to.not.be.null;
+      }).catch((err) => {
+        console.log(err);
+      });
+
+    });
+
+  });
+
+  describe('when I fetch data it', function () {
+    it('should return world data', function (done) {
+
+      map.fetchData()
+        .then((resp) => {
+          expect(resp).to.be.a('object');
+          expect(resp).to.have.deep.property('objects.countries');
+          done();
+        })
+        .catch((err) => {
+          done(err);
+        });
+    });
+
+    it('should create an svg from the world data', function (done) {
+      map.fetchData()
+        .then(map.createSVG)
+        .then((data) => {
+          done();
+        }).catch((err) => {
+          done(err);
+        });
+    });
+
+    it('should resolve with an object with { map, svg } properties', function (done) {
+      map.fetchData()
+        .then(map.createSVG)
+        .then((data) => {
+          expect(data).to.have.property('map');
+          expect(data).to.have.property('svg');
+          done();
+        }).catch((err) => {
+          done(err);
+        });
     });
 
   });
